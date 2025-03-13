76bc006674ce084f264afa5f243d2ebf51b76790
Casper513 <iqmamgrentoger@gmail.com>
2025-03-13 16:31:51 +0700

    improve layout

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,4 @@
-<x-guest-layout>
+
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
@@ -54,4 +54,3 @@
             </p>
         </div>
     </form>
-</x-guest-layout>
