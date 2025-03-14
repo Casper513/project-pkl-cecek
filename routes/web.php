1232ddb77da1b8e9b73bdf1ffc0f7be6fc6aeb72
Casper513 <iqmamgrentoger@gmail.com>
2025-03-14 18:56:16 +0700

    add route

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,6 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-
-
 Route::get('/dashboard', [DashboardController::class, 'index'])
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
@@ -14,13 +12,23 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// Route untuk Register & Login
+Route::get('/register', function () {
+    return view('auth.register');
+})->name('register');
+
+Route::get('/login', function () {
+    return view('auth.login');
+})->name('login');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::get('/presensi/create' , function () {
+    Route::get('/presensi/create', function () {
         return view('Presensi.create');
     });
 });
 
-require __DIR__.'/auth.php';
+// Menggunakan auth routes bawaan Laravel
+require __DIR__ . '/auth.php';
